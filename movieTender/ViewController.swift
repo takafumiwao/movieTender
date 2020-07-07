5d3f1d4f6d7c9d4fa0e07bd55c5834b4ac50ceef
takafumiwao <koshiiwao2904@icloud.com>
2020-07-07 10:19:08 +0900

    Initial Commit

---
+++ movieTender/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movieTender
+//
+//  Created by 岩男高史 on 2020/07/07.
+//  Copyright © 2020 岩男高史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
