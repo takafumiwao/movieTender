2e79943dd46a4628180153756b626cf2e329a293
takafumiwao <koshiiwao2904@icloud.com>
2020-07-08 17:44:53 +0900

    #3 メイン画面の作成

---
+++ movieTender/Controller/MainController/TrendViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TrendViewController.swift
+//  movieTender
+//
+//  Created by 岩男高史 on 2020/07/07.
+//  Copyright © 2020 岩男高史. All rights reserved.
+//
+
+import UIKit
+
+class TrendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+}
---
+++ movieTender/Enum/Enum.swift
@@ -0,0 +1,40 @@
+//
+//  Feel.swift
+//  movieTender
+//
+//  Created by 岩男高史 on 2020/07/08.
+//  Copyright © 2020 岩男高史. All rights reserved.
+//
+
+import Foundation
+
+enum FeelType: String {
+    case Cry = "Cry"
+    case Disgust = "Disgust"
+    case Joy = "Joy"
+    case Angry = "Angry"
+    case Fear = "Fear"
+    case Suprise = "Suprise"
+}
+
+enum Genres: Int {
+    case action = 28
+    case adventure = 12
+    case animation = 16
+    case comedy = 35
+    case crime = 80
+    case documentary = 99
+    case drama = 18
+    case family = 10751
+    case fantasy = 14
+    case history = 36
+    case horror = 27
+    case music = 10402
+    case mystery = 9648
+    case romance = 10749
+    case scienceFiction = 878
+    case tvMovie = 10770
+    case thriller = 53
+    case war = 10752
+    case western = 37
+}
